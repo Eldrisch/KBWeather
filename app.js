a6e54ef452c8d31779502910af5c4ff859cae258
Eldrisch <bara1211@gmail.com>
2018-10-25 22:29:15 +0200

    Environment Setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'src')));
+app.use('*/img', express.static(path.join(__dirname, 'src/images')));
+app.use('*/js', express.static(path.join(__dirname, 'src/scripts')));
+app.use('*/css', express.static(path.join(__dirname, 'src/styles')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
